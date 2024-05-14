cae8dff5eb43c45bc867c75f4051d9f19585e783
Watcharapong.K <147409902+Voltran1c@users.noreply.github.com>
2024-05-14 13:20:19 +0700

    Js if-else1 commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const dayNumber = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday"];
+
+if (dayNumber) {
+  console.log("This is "+ dayNumber[2]);
+} else {
+  console.log("Not day of the week");
+}
